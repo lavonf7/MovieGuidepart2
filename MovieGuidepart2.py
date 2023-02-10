69f68d52917ee48d88f256a304e644012dec9d37
lavonf7 <lavontfields@gmail.com>
2023-02-09 17:21:47 -0800

    Add project files.

---
+++ MovieGuidepart2.py
@@ -0,0 +1,35 @@
+FILENAME = "movies.txt"
+
+def write_movies(movies):
+    with open(FILENAME, "w") as file:
+        for movie in movies:
+            file.write(f"{movie}\n")    
+
+def read_movies():
+    movies = []
+    with open(FILENAME) as file:
+        for line in file:
+            line = line.replace("\n", "")
+            movies.append(line)
+    return movies   
+
+def list_movies(movies):
+    for i, movie in enumerate(movies, start=1):
+        print(f"{i}. {movie}")
+    print()
+  
+def add_movie(movies):
+    movie = input("Movie: ")
+    movies.append(movie)
+    write_movies(movies)
+    print(f"{movie} was added.\n")
+
+def delete_movie(movies):
+    index = int(input("Number: "))
+    if index < 1 or index > len(movies):
+        print("Invalid movie number.\n")
+    else:
+        movie = movies.pop(index - 1)
+        write_movies(movies)
+        print(f"{movie} was deleted.\n")
+
